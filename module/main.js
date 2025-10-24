83a5b25df7644a5f6ee78c1997c07d459c31af42
muhammad-husnain-ali-web <mhusnainali913@gmail.com>
2025-10-24 18:15:06 +0500

    module

---
+++ module/main.js
@@ -0,0 +1,35 @@
+// // const http = require("node:http");
+
+// import http from "http";
+// const hostname = "127.0.0.1";
+
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     // res.setHeader("Content-Type", "text/plain");
+//     res.setHeader("Content-Type", "text/html");
+//     res.end("<h1>  Hello World Husnain </h1>");
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`)
+// })
+
+// import {a, b, c, d, e} from "./mymodule.js";
+// console.log(a,b, c, d, e);
+
+// import obj from "./mymodule.js";
+// console.log(obj);
+
+// import Husnain from "./mymodule.js";
+// console.log(Husnain);
+
+// common js rap
+// (function(exports, require, module, __filename, __dirname){
+        //  in this function
+// });
+// const a = require("./mymodule2")
+// console.log(a)
+// console.log(__dirname)
+// console.log(__filename)
---
+++ module/mymodule2.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//     a:1,
+//     b:4
+// };
+
+// a = "a value"
+
+// module.exports = a;
+x = {
+    a:1,
+    b:4
+};
+
+module.exports = x;
