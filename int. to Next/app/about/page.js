a9ab0734b3c8ebb303e10dd3062ef049c7481195
muhammad-husnain-ali-web <mhusnainali913@gmail.com>
2025-11-19 18:11:36 +0500

    int. to Next

---
+++ int. to Next/app/about/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <div>
+            {/* I am about page */}
+            <h1 className="text-xl">Problem solved by Next js</h1>
+            <ul>
+              <li >Full Stack Solution</li>
+              <li>File based routing</li>
+              <li>Aditional features like router from next/nevigation</li>
+              <li>Optimized rendering</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default about
---
+++ int. to Next/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        I am a Navbar
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
